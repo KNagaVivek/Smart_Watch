14eb0c56e60c6c98cb5e4c6646fe429a8936ca61
KNagaVivek <79193329+KNagaVivek@users.noreply.github.com>
2022-07-13 20:28:31 +0530

    Updated

--- SMART-WATCH.ino
+++ SMART-WATCH.ino
@@ -1,3 +1,9 @@
+//Below 3 are related blynk 
+#define BLYNK_TEMPLATE_ID "TMPLB1HnvOp2"
+#define BLYNK_DEVICE_NAME "Smart Watch GPS"
+#define BLYNK_AUTH_TOKEN "V3uWIiY_L8B-fryeKyLvi56IZWysIIaw"
+
+
 #include <driver/rtc_io.h>
 #include <WiFi.h>
 #include "time.h"
@@ -6,44 +12,49 @@
 #include <TinyGPS++.h>
 #include "twilio.hpp"
 #include "ThingSpeak.h";
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
 
-#define BUTTON_PIN 19
+
+
+#define BUTTON_PIN 19            //defining the push button pin
+//Next 2 lines are id & api key for Thingspeak
 #define TS_Channel_ID 1679754
 #define TS_API "ZCAZXFTOSJJO6MU9"
 
 
-SSD1306 display(0x3c, 21, 22);
+SSD1306 display(0x3c, 21, 22);  //definig the oled display with its address,SCL & SDA connected pins
 int c = 0;
 int ls = LOW;  // the previous state from the input pin
 int cs;     // the current reading from the input pin
 
 WiFiClient client;
 
+char auth[] = BLYNK_AUTH_TOKEN;  
 
 static const char *ssid = "V2037";
 static const char *password = "lokesh@123";
 
-//static const char *account_sid = "AC6296e127859b6735be7bb5a45ad2a5a6";
-//static const char *account_sid = "AC1a3e0e5fdb382fc7adb6444a2b370bd8";
-static const char *account_sid = "ACf96ce7f1be0906af105b6f9e85005293";
-//static const char *auth_token = "66de627e23592b3d0d92751fb313f710";
-//static const char *auth_token = "a3be44ebba590dfc1afbc26c709b1f67";
-static const char *auth_token = "0dff126c7e715b049ef9565ecd0cba66";
+
+//Below are related to Twilio
+static const char *account_sid = "AC3ab99d430b690ba2548f98a2b7e744b3";
+static const char *auth_token = "ab2746b72c2eb47e4fd1d3d112bbe29f";
 // Phone number should start with "+<countrycode>"
-//static const char *from_number = "+19107668616";
-//static const char *from_number = "+16603337536";
-static const char *from_number = "+14709446734";
+static const char *from_number = "+19896933507";
 
 
 // You choose!
 // Phone number should start with "+1910"
-static const char *to_number = "+919160401792";
+static const char *to_number = "+918247463959";
 String message = "Sent from ESP32:\n";
 
+
+//For displaying the time we use
 const char* ntpServer = "pool.ntp.org";
 const long gmtOffset_sec = 19800;
 const int  daylightOffset_sec = 0;
 
+char lati[10],loni[10];
 
 #define RXD2 16
 #define TXD2 17
@@ -53,6 +64,7 @@ TinyGPSPlus gps;
 
 Twilio *twilio;
 
+BlynkTimer timer;
 
 void printLocalTime()
 {
@@ -67,9 +79,13 @@ void printLocalTime()
   int d = timeinfo.tm_mday;
   int mn = timeinfo.tm_mon;
   int y = timeinfo.tm_year;
+  int fl = 0;
   mn = mn+1;
   y = 1900+y;
-
+  if(h>12) {
+    h-=12;
+    fl = 1;
+  }
   static char hr[15];
   static char minn[15];
   static char dayy[15];
@@ -86,9 +102,11 @@ void printLocalTime()
   display.clear();
   //display.setRotation(2); 
   display.setFont(ArialMT_Plain_24);
-  display.drawString(26,10,hr);
-  display.drawString(59,10,":");
-  display.drawString(72,10,minn);
+  display.drawString(10,10,hr);
+  display.drawString(37,10,":");
+  display.drawString(50,10,minn);
+  if(fl==0) display.drawString(82,10,"AM");
+  else display.drawString(82,10,"PM");
 
   display.setFont(ArialMT_Plain_10);
   display.drawString(30,36,dayy);
@@ -96,6 +114,26 @@ void printLocalTime()
   display.drawString(62,36,yr);
   display.display();
   delay(2000);
+}
+
+void senddata() {
+  if(checkGPS()) {
+    Blynk.virtualWrite(V0, lati);
+    Blynk.virtualWrite(V1, loni);
+    Serial.println(lati);
+    Serial.println(loni);
+  }
+}
+
+boolean checkGPS() {
+  for (unsigned long start = millis(); millis() - start < 1000;){
+      while (neogps.available()){
+        if (gps.encode(neogps.read())) {
+          return true;
+        }
+      }
+    }
+    return false;
 }
 
 
@@ -116,8 +154,8 @@ void setup() {
       ESP.restart();
     }
   }
+  Blynk.begin(auth,ssid, password);
   Serial.println("CONNECTED");
-  
   pinMode(BUTTON_PIN, INPUT_PULLUP);
   
   twilio = new Twilio(account_sid, auth_token);
@@ -145,25 +183,16 @@ void setup() {
 }
 
 void loop() {
+  timer.setInterval(1000L, senddata);
   cs=digitalRead(BUTTON_PIN);
   if(ls==HIGH && cs==LOW) {
-    boolean newData = false;
-      for (unsigned long start = millis(); millis() - start < 1000;)
-  {
-    while (neogps.available())
-    {
-      if (gps.encode(neogps.read()))
-      {
-        newData = true;
-      }
-    }
-  }
-
+    boolean newData = checkGPS();
+    
   if(newData == true) {
     newData = false;
     Serial.println(gps.satellites.value());
     print_speed();
-  } else {
+   } else {
     Serial.println("No Data");
     display.clear();
     display.setFont(ArialMT_Plain_24);
@@ -173,6 +202,8 @@ void loop() {
   }  
  } 
  else {
+  Blynk.run();
+  timer.run();
   printLocalTime(); 
  }
  ls=cs;
@@ -197,7 +228,6 @@ void print_speed() {
     Serial.print("ALT:");
     Serial.println(gps.altitude.meters(), 0);
 
-    char lati[10],loni[10];
     dtostrf(gps.location.lat(),2,6,lati);
     dtostrf(gps.location.lng(),2,6,loni);
     
